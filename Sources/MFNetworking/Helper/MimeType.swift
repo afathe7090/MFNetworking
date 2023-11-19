a3dafa39070fda2694fe55358a565a3e82181b87
Ahmed Fathy <afathe7090>
2023-11-19 23:46:45 +0200

    - add multiple mime types for upload

--- Sources/MFNetworking/Helper/MimeType.swift
+++ Sources/MFNetworking/Helper/MimeType.swift
@@ -8,17 +8,42 @@
 import Foundation
 
 public enum MimeType: String {
+    // Images
     case jpeg = "image/jpeg"
     case png = "image/png"
     case gif = "image/gif"
     case bmp = "image/bmp"
     case webp = "image/webp"
     case tiff = "image/tiff"
+
+    // Audio
     case mp3 = "audio/mpeg"
     case wav = "audio/wav"
     case ogg = "audio/ogg"
-    case mp4 = "video/mp4"
     case m4a = "audio/mp4"
+    case aac = "audio/aac"
+    case flac = "audio/flac"
+
+    // Video
+    case mp4 = "video/mp4"
     case mov = "video/quicktime"
     case avi = "video/x-msvideo"
+    case mkv = "video/x-matroska"
+    case webm = "video/webm"
+    case wmv = "video/x-ms-wmv"
+
+    // Documents
+    case pdf = "application/pdf"
+    case doc = "application/msword"
+    case docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    case xls = "application/vnd.ms-excel"
+    case xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    case ppt = "application/vnd.ms-powerpoint"
+    case pptx = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+
+    // Other types
+    case txt = "text/plain"
+    case html = "text/html"
+    case xml = "text/xml"
+    case json = "application/json"
 }
